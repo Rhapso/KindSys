30576a2aeb0381ba712312f6219f536850d56834
2014-0624-1315\Administrator <1105127465@qq.com>
2017-04-14 22:24:14 +0800

    add news router
    Signed-off-by: 2014-0624-1315\Administrator <1105127465@qq.com>

--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,7 @@ import Router from 'vue-router'
 import Main from '@/components/Main'
 import Home from '@/components/home/Home'
 import Teachers from '@/components/home/Teachers'
-
+import NewsList from '@/components/home/news/news-list'
 Vue.use(Router)
 
 export default new Router({
@@ -17,6 +17,11 @@ export default new Router({
       path: '/home',
       name: 'Home',
       component: Home
+    },
+    {
+      path: '/news/7/0',
+      name: 'News',
+      component: NewsList
     },
     {
       path: '/teachers',
