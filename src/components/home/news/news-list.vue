4b1272d0151aed00df606985602432ae3cde374f
2014-0624-1315\Administrator <1105127465@qq.com>
2017-04-14 22:17:11 +0800

    add news
    Signed-off-by: 2014-0624-1315\Administrator <1105127465@qq.com>

---
+++ src/components/home/news/news-list.vue
@@ -0,0 +1,78 @@
+<template>
+  <div class="info-container">
+    <div class="info-item">
+      <div class="info-photo">
+        <img  src="/static/imgs/photo1.jpg" />
+      </div>
+      <div class="info-post">
+        <h2 class="info-title">童心献爱 温暖老人心</h2>
+        <div class="info-data">09月27日</div>
+        <div class="info-readers">19阅读</div>
+      </div>
+    </div>
+    <div class="info-item">
+      <div class="info-photo">
+        <img  src="/static/imgs/photo2.jpg" />
+      </div>
+      <div class="info-post">
+        <h2 class="info-title">童心献爱 温暖老人心</h2>
+        <div class="info-data">09月27日</div>
+        <div class="info-readers">19阅读</div>
+      </div>
+    </div>
+    <div class="info-item">
+      <div class="info-photo">
+        <img src="/static/imgs/photo3.jpg" />
+      </div>
+      <div class="info-post">
+        <h2 class="info-title">童心献爱 温暖老人心</h2>
+        <div class="info-data">09月27日</div>
+        <div class="info-readers">19阅读</div>
+      </div>
+    </div>
+    <div class="info-item">
+      <div class="info-photo">
+        <img src="/static/imgs/photo4.jpg" />
+      </div>
+      <div class="info-post">
+        <h2 class="info-title">童心献爱 温暖老人心</h2>
+        <div class="info-data">09月27日</div>
+        <div class="info-readers">19阅读</div>
+      </div>
+    </div>
+    <div class="info-item">
+      <div class="info-photo">
+        <img  src="/static/imgs/photo5.jpg" />
+      </div>
+      <div class="info-post">
+        <h2 class="info-title">童心献爱 温暖老人心</h2>
+        <div class="info-data">09月27日</div>
+        <div class="info-readers">19阅读</div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+  export default {
+    name: 'NewsList',
+    data () {
+    }
+  }
+</script>
+<style scoped>
+  .info-item{
+    padding:10px 10px 10px 15px;
+  }
+  .info-photo{
+    width: 100px;
+    height:100px;
+    float: left;
+    margin-right: 20px;
+  }
+  img{
+    width:100%;
+  }
+  .info-post{
+    height:100px;
+  }
+</style>
