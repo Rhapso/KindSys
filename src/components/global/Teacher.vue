7914039079661c785216b80e98d102e7bcf0e676
haoliang <1259799112@qq.com>
2017-04-23 20:31:21 +0800

    add MyClass and some others

---
+++ src/components/global/Teacher.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="teacher">
+    <div class="wraper">
+      <div> <img class="icon" v-bind:src="url" /> </div>
+      <Card> <p style="width:50%; display:inline;"> 身份: </p> <p style="width:50%; float:right; text-align:right;"> {{ name }}老师 </p> </Card>
+      <Card> <p style="width:50%; display:inline;"> 任教科目: </p> <p  style="width:50%; float:right; text-align:right;"> {{ subject }} </p> </Card>
+      <div class="button"> <Button style="background:#3399ff;">leave message</Button> </div>
+      <div class="button"> <Button style="background:#00cc66;">phone call</Button> </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'teacher',
+    data () {
+      return {
+        url: '/static/imgs/icon1.jpg',
+        name: 'foo',
+        subject: 'math'
+      }
+    }
+  }
+</script>
+
+<style scoped>
+div.teacher { margin-top: 30px; width:400px; position:absolute; left:50%; }
+div.wraper { width: 400px; position:absolute; left:-50%; }
+img.icon { width:100px; height:100px; margin-left:150px; }
+.button Button { width:300px; margin-left:50px; margin-top:20px; font-size:20px; }
+</style>
---
+++ src/components/myClass/ClassMember.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="classMember">
+
+    <Row :gutter="16">
+      <Col span="6" class="col" v-for="member in memberList" :key="member.id">
+        <div class="member">
+          <a v-bind:href="member.ref">
+          <div> <img class="icon" v-bind:src="member.url" /> </div>
+          </a>
+          <p class="name"> {{ member.name }} </p>
+        </div>
+      </Col>
+    </Row>
+
+    <a href="#/myClass/classInfo"> <Button class="info">
+      <div>
+        <h1> {{ classType }} ({{ classNum }}) 班 </h1>
+        <p> 代码: {{code}} </p>
+      </div>
+    </Button> </a>
+    <Button class="quit">退出班级</Button>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'classMember',
+    data () {
+      return {
+        classType: 'small',
+        classNum: 1,
+        code: 24678,
+        memberList: [
+          {
+            ref: '#/Teacher',
+            url: '/static/imgs/icon1.jpg',
+            name: 'teacher_foo'
+          },
+          {
+            ref: '#/Parent',
+            url: '/static/imgs/icon2.jpg',
+            name: 'parent_bar'
+          },
+          {
+            ref: '#/Teacher',
+            url: '/static/imgs/icon1.jpg',
+            name: 'teacher_boo'
+          },
+          {
+            ref: '#/Parent',
+            url: '/static/imgs/icon2.jpg',
+            name: 'parent_far'
+          },
+          {
+            ref: '#/Teacher',
+            url: '/static/imgs/icon1.jpg',
+            name: 'teacher_foobar'
+          }
+        ]
+      }
+    }
+  }
+</script>
+
+<style scoped>
+Col.col { margin:0; padding:0; }
+Button.info { width:100%; margin-top:60px; text-align:center; }
+.info div { padding:10px; }
+Button.quit { width:100%; font-size:20px; padding:10px; margin-top:30px; }
+
+.member { width:100%; height:100%; margin-top:20px; margin-bottom:0; }
+.member img.icon { width:80%; height:80%; margin-left:10%; margin-right:10%; }
+.member p.name { width:100%; text-align:center; margin-top:0; font-size:20px; }
+</style>
