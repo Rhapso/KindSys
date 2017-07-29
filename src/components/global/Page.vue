7294dfa99c6e7ce513e1bb6840b6f6c51c35aecd
Rhapso <your_mr_fan@sjtu.edu.cn>
2017-07-29 20:21:31 +0800

    adding Info.vue

---
+++ src/components/global/Page.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+        <group gutter='0'>
+		    <cell title="联系我们"></cell>
+		    <cell :border-intent="false" primary='content'>
+			    <x-textarea :height="200" style="padding:0" :placeholder="placeholder" @on-focus="onEvent('focus')" @on-blur="onEvent('blur')" ></x-textarea>
+		    </cell>
+	    </group> 
+        <group>
+	       <x-button type="primary" action-type="button">提交</x-button>
+        </group> 
+    </div>
+</template>
+
+<script>
+import { XTextarea, Group, Cell, XInput, XButton, DatetimeRange } from 'vux'
+
+export default {
+  name: 'Page',
+  data () {
+    return {
+      placeholder: '您有什么建议?'
+    }
+  },
+  components: {
+    XTextarea,
+    Group,
+    Cell,
+    XInput,
+    XButton,
+    DatetimeRange
+  },
+  methods: {
+    onChange (value) {
+      console.log('change', value)
+    },
+    onEvent (event) {
+      console.log('on', event)
+    }
+  }
+}
+</script>
